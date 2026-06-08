191c0993a45ba20affe92329381edc1e742b67ab
mosin <mosinmd827@gmail.com>
2026-06-08 22:15:24 +0530

    feat: Add Vercel Web Analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import SmoothScroll from "@/components/SmoothScroll";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <SmoothScroll>
     <App />
+    <Analytics />
   </SmoothScroll>,
 );
